7fdd7dbe5b11c6a5712c7b732b8280d9d49b5a94
Khoshimov <khashimov023@gmail.com>
2022-10-15 13:23:44 +0500

    Initial Commit

---
+++ MVVM-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-RxSwift
+//
+//  Created by Shavkat Khoshimov on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
